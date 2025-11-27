2434bc1cf1088b6cbdce929c027de0f2af80f6c9
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-11-27 10:49:47 +0900

    test: should return OK to CORS-allowed origins and Forbidden otherwise

---
+++ src/main/kotlin/com/matchalab/subscription_killer_api/config/WebSecurityConfig.kt
@@ -0,0 +1,6 @@
+package com.matchalab.subscription_killer_api.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@Configuration @EnableWebSecurity open class WebSecurityConfig {}
---
+++ src/test/kotlin/com/matchalab/subscription_killer_api/config/WebSecurityConfigTest.kt
@@ -0,0 +1,59 @@
+package com.matchalab.subscription_killer_api.config
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.ContextHierarchy
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient
+import org.springframework.web.context.WebApplicationContext
+
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(value = [SpringExtension::class])
+@WebAppConfiguration("classpath:META-INF/web-resources")
+@ContextHierarchy(value = [ContextConfiguration(classes = [WebSecurityConfig::class])])
+class WebSecurityConfigTest @Autowired constructor() {
+
+    @Autowired lateinit var wac: WebApplicationContext
+    lateinit var client: WebTestClient
+
+    @BeforeEach
+    fun setUp() {
+        client = MockMvcWebTestClient.bindToApplicationContext(wac).configureClient().build()
+    }
+
+    @Test
+    fun `should return 403 Forbbien when origin is not allowed by CORS`() {
+        client.get()
+                .uri("/ping")
+                .header(HttpHeaders.ORIGIN, "https://www.google.com/")
+                .exchange()
+                .expectStatus()
+                .isForbidden()
+                .expectHeader()
+                .valueEquals("accessControlAllowOrigin", null)
+    }
+
+    @Test
+    fun `should return 200 OK when origin is allowed by CORS`() {
+        client.get()
+                .uri("/ping")
+                .header(
+                        HttpHeaders.ORIGIN,
+                        "https://subscription-killer-git-main-matchalab-project.vercel.app"
+                )
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .valueEquals(
+                        "accessControlAllowOrigin",
+                        "https://subscription-killer-git-main-matchalab-project.vercel.app"
+                )
+    }
+}
