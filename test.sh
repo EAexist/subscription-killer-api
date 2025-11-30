e310c41d3f67888ee41507b07b4df614fd560441
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-11-30 21:15:04 +0900

    chore(scripts): Update test runner script to accept test class argument

--- test.sh
+++ test.sh
@@ -1,4 +1,17 @@
-docker compose up -d && \
-DB_ENDPOINT=localhost DB_NAME=subscription_killer_db DB_USER=guest DB_PASSWORD=hello_guest \
-./gradlew test -Dspring.profiles.active=local,development && \
+#!/bin/bash
+TEST_CLASSES=("$@")
+
+docker compose up -d 
+export DB_ENDPOINT=localhost
+export DB_NAME=subscription_killer_db
+export DB_USER=guest
+export DB_PASSWORD=hello_guest
+
+if [ ${#TEST_CLASSES[@]} -eq 0 ]; then
+    ./gradlew test -Dspring.profiles.active=local,development
+else
+    TEST_ARGS=$(printf "'%s' " "${TEST_CLASSES[@]}")
+    ./gradlew test -Dspring.profiles.active=local,development --tests $TEST_ARGS
+fi
+
 docker compose down
