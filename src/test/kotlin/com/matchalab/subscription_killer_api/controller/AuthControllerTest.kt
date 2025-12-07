e021c567647c22be85e482df2bb71f7cc4992dc6
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-12-08 06:27:43 +0900

    test: disable AuthControllerTest (GoogleTokenAuthFilterIntegrationTest covers it.)

--- src/test/kotlin/com/matchalab/subscription_killer_api/controller/AuthControllerTest.kt
+++ src/test/kotlin/com/matchalab/subscription_killer_api/controller/AuthControllerTest.kt
@@ -7,11 +7,8 @@ import com.matchalab.subscription_killer_api.core.dto.LoginRequestDto
 import com.matchalab.subscription_killer_api.service.AuthService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.security.authentication.AuthenticationManager
@@ -27,7 +24,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 
 private val logger = KotlinLogging.logger {}
 
-@WebMvcTest(AuthController::class, excludeAutoConfiguration = [SecurityAutoConfiguration::class])
+// @WebMvcTest(AuthController::class, excludeAutoConfiguration = [SecurityAutoConfiguration::class])
 @AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest() {
 
@@ -57,7 +54,7 @@ class AuthControllerTest() {
     //             )
     // }
 
-    @Test
+    // @Test
     fun `should load Jackson in test context`() {
         logger.debug { "mockMvc: $mockMvc" }
         logger.debug { "Jackson modules: " + objectMapper.registeredModuleIds }
@@ -77,7 +74,7 @@ class AuthControllerTest() {
         }
     }
 
-    @Test
+    // @Test
     @WithMockUser
     fun `should return AppUserDTO and HttpStatus provided by AuthService when requested login`() {
         val requestBody: LoginRequestDto = LoginRequestDto(idToken = "FAKE_VALID_FIRST_SEEN_TOKEN")
