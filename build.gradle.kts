2191f0e5d0b3d403d439f35c5c53bb4fed05ff0f
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-12-08 23:44:19 +0900

    chore(deps): mockito-kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,6 +65,10 @@ dependencies {
     
     // https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-test/
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+
+    // https://mvnrepository.com/artifact/org.mockito.kotlin/mockito-kotlin
+    testImplementation("org.mockito.kotlin:mockito-kotlin:6.1.0")
+    
 }
 
 kotlin { compilerOptions { freeCompilerArgs.addAll("-Xjsr305=strict") } }
