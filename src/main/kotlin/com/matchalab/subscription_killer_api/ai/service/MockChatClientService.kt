6d33a9b2101664908765dc13ced4d653de529d6b
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-01-14 05:27:37 +0900

    feat: MockChatClientService returns mocked result for EmailCategorization, EmailTemplateExtraction

--- src/main/kotlin/com/matchalab/subscription_killer_api/ai/service/MockChatClientService.kt
+++ src/main/kotlin/com/matchalab/subscription_killer_api/ai/service/MockChatClientService.kt
@@ -2,10 +2,10 @@ package com.matchalab.subscription_killer_api.ai.service
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.matchalab.subscription_killer_api.ai.dto.EmailCategorizationResponse
+import com.matchalab.subscription_killer_api.ai.dto.EmailTemplateExtractionResponse
+import com.matchalab.subscription_killer_api.ai.dto.EmailTemplateExtractionResult
 import com.matchalab.subscription_killer_api.subscription.EmailTemplate
-import com.matchalab.subscription_killer_api.subscription.SubscriptionEventType
-import com.matchalab.subscription_killer_api.subscription.service.EmailDetectionRuleGenerationDto
-import com.matchalab.subscription_killer_api.subscription.service.UpdateEmailDetectionRulesFromAIResultDto
+import com.matchalab.subscription_killer_api.subscription.service.EmailCategorizationResponseFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
@@ -25,40 +25,55 @@ class MockChatClientService(
     ): T {
         val promptTemplate: String = promptTemplateStream.getContentAsString(Charsets.UTF_8).trimIndent()
 
-        if ("{emails}" in promptTemplate) {
-            return EmailCategorizationResponse(
-                listOf(),
-                listOf(),
-                listOf(),
-                listOf(),
+        if ("""
+            Output format (exact keys):
+            \{
+               "M": ["ID1", "ID2"],
+               "A": [],
+               "S": ["ID3"],
+               "C": ["ID4"]
+            \}
+        """.trimIndent() in promptTemplate
+        ) {
+            val emailCategorizationResponse: EmailCategorizationResponse =
+                EmailCategorizationResponseFactory.createSample()
+            return mapOf(
+                "M" to emailCategorizationResponse.subsStartMsgIds,
+                "A" to emailCategorizationResponse.subsCancelMsgIds,
+                "S" to emailCategorizationResponse.monthlyMsgIds,
+                "C" to emailCategorizationResponse.annualMsgIds,
             ) as T
         }
 
-        if ("{categorizedEmails}" in promptTemplate) {
-            return UpdateEmailDetectionRulesFromAIResultDto(null, null, null, null) as T
-        }
-
-        if ("{currentMonthlyPaymentRule}" in promptTemplate) {
-            return UpdateEmailDetectionRulesFromAIResultDto(
-                EmailDetectionRuleGenerationDto(
-                    SubscriptionEventType.SUBSCRIPTION_START,
+        if ("""
+            Output format (exact keys):
+            \{
+              "result": [
+                \{
+                  "messageIds": ["123","456"],
+                  "template": \{
+                    "subjectRegex": "regex-for-subject",
+                    "snippetRegex": "regex-for-snippet"
+                  \}
+                \}
+              ]
+            \}
+        """.trimIndent() in promptTemplate
+        ) {
+            val result = (params["emails"] as String).lines().map { line ->
+                val parts = line.split("|")
+                EmailTemplateExtractionResult(
+                    listOf(parts[0]),
                     EmailTemplate(
-                        "계정 정보 변경",
-                        "새로운 결제 수단 정보"
+                        parts[1],
+                        parts[2]
                     )
-                ),
-                EmailDetectionRuleGenerationDto(
-                    SubscriptionEventType.SUBSCRIPTION_CANCEL,
-                    EmailTemplate(
-                        "결제 수단을 업데이트",
-                        "멤버십이 현재 정지"
-                    )
-                ),
-                null,
-                null
+                )
+            }
+            return EmailTemplateExtractionResponse(
+                result
             ) as T
         }
-
         return responseType.getDeclaredConstructor().newInstance()
     }
 
