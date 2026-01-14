c3e0e9aed6b654683298fd50589731ba7eb46ce3
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-01-15 00:25:50 +0900

    test(ai-token): refactor Eval to take unique emails' output of categorization task

--- src/test/kotlin/com/matchalab/subscription_killer_api/evals/prompts/GeneralizeStringPatternEval.kt
+++ src/test/kotlin/com/matchalab/subscription_killer_api/evals/prompts/GeneralizeStringPatternEval.kt
@@ -3,28 +3,20 @@ package com.matchalab.subscription_killer_api.evals.prompts
 import com.matchalab.subscription_killer_api.ai.dto.EmailTemplateExtractionResponse
 import com.matchalab.subscription_killer_api.ai.dto.toEmailDetectionRuleGenerationDto
 import com.matchalab.subscription_killer_api.ai.dto.toMessages
-import com.matchalab.subscription_killer_api.ai.service.ChatClientServiceImpl
-import com.matchalab.subscription_killer_api.ai.service.config.AiConfig
-import com.matchalab.subscription_killer_api.ai.service.config.PromptTemplateProperties
 import com.matchalab.subscription_killer_api.subscription.GmailMessage
 import com.matchalab.subscription_killer_api.subscription.SubscriptionEventType
-import com.matchalab.subscription_killer_api.subscription.config.MailProperties
-import com.matchalab.subscription_killer_api.subscription.config.SampleMessageConfig
 import com.matchalab.subscription_killer_api.subscription.matchMessagesOrEmpty
 import com.matchalab.subscription_killer_api.subscription.service.EmailCategorizationResponseFactory
 import com.matchalab.subscription_killer_api.subscription.service.EmailDetectionRuleGenerationDto
 import com.matchalab.subscription_killer_api.subscription.service.EmailTemplateExtractionPromptService
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.micrometer.observation.Observation
+import io.micrometer.observation.ObservationRegistry
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.assertAll
-import org.springframework.ai.model.chat.client.autoconfigure.ChatClientAutoConfiguration
-import org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration
-import org.springframework.ai.model.tool.autoconfigure.ToolCallingAutoConfiguration
-import org.springframework.ai.retry.autoconfigure.SpringAiRetryAutoConfiguration
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.Test
 
@@ -32,34 +24,30 @@ private val logger = KotlinLogging.logger {}
 
 @Tag("ai")
 @SpringBootTest(
-    classes = [
-        ToolCallingAutoConfiguration::class,
-        GoogleGenAiChatAutoConfiguration::class,
-        ChatClientAutoConfiguration::class,
-        SpringAiRetryAutoConfiguration::class,
-        EmailTemplateExtractionPromptService::class,
-        JacksonAutoConfiguration::class,
-        ChatClientServiceImpl::class,
-        AiConfig::class,
-        SampleMessageConfig::class
-    ],
-    webEnvironment = SpringBootTest.WebEnvironment.NONE
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-@EnableConfigurationProperties(PromptTemplateProperties::class, MailProperties::class)
+@AutoConfigureObservability
 class GeneralizeStringPatternEval @Autowired constructor(
     private val emailTemplateExtractionPromptService: EmailTemplateExtractionPromptService,
     private val sampleMessages: List<GmailMessage>,
+    private val observationRegistry: ObservationRegistry
 ) {
 
     @Test
     fun `given gmail messages should create general regex patterns for subscription event categories`() {
 
-        val emailCategorizationResponse = EmailCategorizationResponseFactory.createSample()
+        val allExpectedEmailCategorizationResponse = EmailCategorizationResponseFactory.createSample()
+
+        val emailCategorizationResponse = EmailCategorizationResponseFactory.createUniqueSample()
 
         val subscriptionEventMessages = emailCategorizationResponse.toMessages(sampleMessages)
 
-        val exactResponse: EmailTemplateExtractionResponse =
-            emailTemplateExtractionPromptService.run(subscriptionEventMessages)
+        lateinit var exactResponse: EmailTemplateExtractionResponse
+
+        Observation.createNotStarted("EmailCategorizationEval", observationRegistry).observe {
+            exactResponse =
+                emailTemplateExtractionPromptService.run(subscriptionEventMessages)
+        }
 
         logger.debug { exactResponse.result.joinToString("\n") { "\n\t${it.messageIds}\n\t${it.template}" } }
 
@@ -75,21 +63,21 @@ class GeneralizeStringPatternEval @Autowired constructor(
                     emailDetectionRuleGenerationDtos.first { it.eventType == SubscriptionEventType.SUBSCRIPTION_START }.template
                 assertThat(template.matchMessagesOrEmpty(sampleMessages).map { it.id })
                     .`as`("SUBSCRIPTION_START")
-                    .containsExactlyInAnyOrderElementsOf(emailCategorizationResponse.subsStartMsgIds)
+                    .containsExactlyInAnyOrderElementsOf(allExpectedEmailCategorizationResponse.subsStartMsgIds)
             },
             {
                 val template =
                     emailDetectionRuleGenerationDtos.first { it.eventType == SubscriptionEventType.SUBSCRIPTION_CANCEL }.template
                 assertThat(template.matchMessagesOrEmpty(sampleMessages).map { it.id })
                     .`as`("SUBSCRIPTION_CANCEL")
-                    .containsExactlyInAnyOrderElementsOf(emailCategorizationResponse.subsCancelMsgIds)
+                    .containsExactlyInAnyOrderElementsOf(allExpectedEmailCategorizationResponse.subsCancelMsgIds)
             },
             {
                 val template =
                     emailDetectionRuleGenerationDtos.first { it.eventType == SubscriptionEventType.MONTHLY_PAYMENT }.template
                 assertThat(template.matchMessagesOrEmpty(sampleMessages).map { it.id })
                     .`as`("MONTHLY_PAYMENT")
-                    .containsExactlyInAnyOrderElementsOf(emailCategorizationResponse.monthlyMsgIds)
+                    .containsExactlyInAnyOrderElementsOf(allExpectedEmailCategorizationResponse.monthlyMsgIds)
             },
             {
                 assertThat(emailDetectionRuleGenerationDtos.none { it.eventType == SubscriptionEventType.ANNUAL_PAYMENT })
