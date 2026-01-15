ad6f27df168ee3ad07779c1f0fca7b2f56857a06
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-01-15 19:23:55 +0900

    feat: deploy artifact to aws lambda

--- infra/create-dummy-zip.sh
+++ infra/create-dummy-zip.sh
@@ -1,1 +1,12 @@
-aws s3 cp dummy.zip s3://subscription-killer-api-stg-deploy/app-package.zip
+#!/bin/bash
+# 1. Create the dummy file
+echo "placeholder" > dummy.txt
+
+# 2. Use 'jar' to create the zip (c=create, v=verbose, f=file)
+jar -cvf subscription-killer-api.zip dummy.txt
+
+# 3. Upload to S3
+aws s3 cp subscription-killer-api.zip s3://subscription-killer-api-stg-deploy/subscription-killer-api.zip
+
+# 4. Cleanup
+rm dummy.txt subscription-killer-api.zip
--- infra/src/main/kotlin/com/matchalab/subscription_killer_api/infra/SubscriptionKillerApiStack.kt
+++ infra/src/main/kotlin/com/matchalab/subscription_killer_api/infra/SubscriptionKillerApiStack.kt
@@ -1,25 +1,42 @@
 //https://docs.aws.amazon.com/ko_kr/lambda/latest/dg/lambda-cdk-tutorial.html#lambda-cdk-step-2
 package com.matchalab.subscription_killer_api.infra
 
+import software.amazon.awscdk.*
+import software.amazon.awscdk.services.lambda.*
+import software.amazon.awscdk.services.lambda.Function
 import software.constructs.Construct
-import software.amazon.awscdk.Stack
-import software.amazon.awscdk.StackProps
+import java.io.File
 
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
-class SubscriptionKillerApiStack(scope: Construct?, id: String?, props: StackProps?) : Stack(scope, id, props) {
+class SubscriptionKillerApiStack(scope: Construct?, id: String?, props: StackProps?, env: String = "stg") :
+    Stack(scope, id, props) {
     constructor(scope: Construct?, id: String?) : this(scope, id, null)
 
     init {
-        val hello = Function.Builder.create(this, "SubscriptionKillerApiFunction")
-            .runtime(Runtime.NODEJS_LATEST)
-            .code(Code.fromAsset("lib/lambda-handler"))
-            .handler("index.handler")
+
+        Tags.of(this).add("Environment", env)
+        Tags.of(this).add("Application", "SubscriptionKillerApi")
+        
+        val artifactPath = File("..", "build/distributions/subscription-killer-api-0.0.1-SNAPSHOT.zip").path
+
+        val handler = Function.Builder.create(this, "Handler")
+            .reservedConcurrentExecutions(10)
+            .functionName("subscription-killer-api-handler-$env")
+            .runtime(Runtime.JAVA_21)
+            .memorySize(2048) // Required for Spring Boot performance
+            .timeout(Duration.minutes(15))
+            .code(
+                Code.fromAsset(artifactPath)
+            )
+            .handler("com.matchalab.subscription_killer_api.StreamLambdaHandler::handleRequest") // Future entry point
             .build()
 
-        LambdaRestApi.Builder.create(this, "ApiGwEndpoint")
-            .restApiName("SubscriptionKillerApi")
-            .handler(hello)
-            .build()
+        val fnUrl = handler.addFunctionUrl(
+            FunctionUrlOptions.builder()
+                .authType(FunctionUrlAuthType.NONE) // Use NONE for public, or IAM for secure
+                .invokeMode(InvokeMode.RESPONSE_STREAM) // CRITICAL for SSE
+                .build()
+        )
+
+        CfnOutput.Builder.create(this, "ApiUrl").value(fnUrl.url).build()
     }
 }
