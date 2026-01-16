b44a3521a0c090efb6402a90aa180a315f857f7c
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-01-16 14:32:35 +0900

    feat(infra): load env variables from aws parameter stroe

--- infra/src/main/kotlin/com/matchalab/subscription_killer_api/infra/SubscriptionKillerApiStack.kt
+++ infra/src/main/kotlin/com/matchalab/subscription_killer_api/infra/SubscriptionKillerApiStack.kt
@@ -4,8 +4,10 @@ package com.matchalab.subscription_killer_api.infra
 import software.amazon.awscdk.*
 import software.amazon.awscdk.services.lambda.*
 import software.amazon.awscdk.services.lambda.Function
+import software.amazon.awscdk.services.ssm.StringParameter
 import software.constructs.Construct
 import java.io.File
+
 
 class SubscriptionKillerApiStack(scope: Construct?, id: String?, props: StackProps?, env: String = "stg") :
     Stack(scope, id, props) {
@@ -15,19 +17,42 @@ class SubscriptionKillerApiStack(scope: Construct?, id: String?, props: StackPro
 
         Tags.of(this).add("Environment", env)
         Tags.of(this).add("Application", "SubscriptionKillerApi")
-        
+
         val artifactPath = File("..", "build/distributions/subscription-killer-api-0.0.1-SNAPSHOT.zip").path
 
+        val appGoogleClientId =
+            StringParameter.valueForStringParameter(this, "/stg/subscription-killer-api/APP_GOOGLE_CLIENT_ID")
+        val appGoogleClientSecret =
+            StringParameter.valueForStringParameter(this, "/stg/subscription-killer-api/APP_GOOGLE_CLIENT_SECRET")
+        val dbEndpoint =
+            StringParameter.valueForStringParameter(this, "/stg/subscription-killer-api/DB_ENDPOINT")
+        val dbName =
+            StringParameter.valueForStringParameter(this, "/stg/subscription-killer-api/DB_NAME")
+        val dbPassword =
+            StringParameter.valueForStringParameter(this, "/stg/subscription-killer-api/DB_PASSWORD")
+        val dbUser =
+            StringParameter.valueForStringParameter(this, "/stg/subscription-killer-api/DB_USER")
+
         val handler = Function.Builder.create(this, "Handler")
-            .reservedConcurrentExecutions(10)
+//            .reservedConcurrentExecutions(10)
             .functionName("subscription-killer-api-handler-$env")
             .runtime(Runtime.JAVA_21)
             .memorySize(2048) // Required for Spring Boot performance
             .timeout(Duration.minutes(15))
+            .handler("com.matchalab.subscription_killer_api.StreamLambdaHandler::handleRequest") // Future entry point
             .code(
                 Code.fromAsset(artifactPath)
             )
-            .handler("com.matchalab.subscription_killer_api.StreamLambdaHandler::handleRequest") // Future entry point
+            .environment(
+                mapOf(
+                    "APP_GOOGLE_CLIENT_ID" to appGoogleClientId,
+                    "APP_GOOGLE_CLIENT_SECRET" to appGoogleClientSecret,
+                    "DB_ENDPOINT" to dbEndpoint,
+                    "DB_NAME" to dbName,
+                    "DB_PASSWORD" to dbPassword,
+                    "DB_USER" to dbUser,
+                )
+            )
             .build()
 
         val fnUrl = handler.addFunctionUrl(
